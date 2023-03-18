8c2d70bc03cbdd6524db6b11516cc78a90f9e414
inigo27 <inigoauz.27@gmail.com>
2023-03-18 18:03:01 +0100

    Last dataset function version

--- week2/kittiMotsDataset.py
+++ week2/kittiMotsDataset.py
@@ -4,7 +4,7 @@ import cv2
 import PIL.Image as Image
 import numpy as np
 
-def read_png_KITTI_MOTS(png_dir, pretrained):
+def read_png_KITTI_MOTS(png_dir, pretrained, seg):
     """
     This functions reads KITTI MOTS annotation png file and returns the
     list of objects in COCO format
@@ -58,36 +58,46 @@ def read_png_KITTI_MOTS(png_dir, pretrained):
             # Get COCO labels id
             # Car
             if class_id == 1:
-                class_id = 3
+                class_id = 2
             # Person
             elif class_id == 2:
-                class_id = 1
+                class_id = 0
         else:
             # To be 0, 1 (COCO format)
             class_id = class_id - 1
         
-        # Polygon
-        contours, _ = cv2.findContours(img_obj, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-        contours = [list(np.squeeze(contour).flatten().astype(float)) for contour in contours if len(contour)>3]
-        
-        if len(contours) != 0:
-        
+        if seg:
+            # Polygon
+            contours, _ = cv2.findContours(img_obj, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+            contours = [list(np.squeeze(contour).flatten().astype(float)) for contour in contours if len(contour)>3]
+            
+            if len(contours) != 0:
+            
+                # Insert in dict
+                obj_dict = {}
+                obj_dict["bbox"] = [xmin, ymin, xmax, ymax]
+                obj_dict["bbox_mode"] = mode
+                obj_dict["category_id"] = class_id
+                obj_dict["segmentation"] = contours
+                
+                objs.append(obj_dict)
+        else:
             # Insert in dict
             obj_dict = {}
             obj_dict["bbox"] = [xmin, ymin, xmax, ymax]
             obj_dict["bbox_mode"] = mode
             obj_dict["category_id"] = class_id
-            obj_dict["segmentation"] = contours
             
             objs.append(obj_dict)
     
     return objs
 
 
-def get_KITTI_MOTS_dataset(img_dir, annot_dir, pretrained = True):
+
+def get_KITTI_MOTS_dataset_bbox(img_dir, annot_dir, pretrained = True):
     """
     This functions generates the dataset in COCO format given the path of the
-    KITTI-MOTS dataset images and annotations
+    KITTI-MOTS dataset images and annotations (only with bboxes)
 
     Parameters
     ----------
@@ -126,7 +136,57 @@ def get_KITTI_MOTS_dataset(img_dir, annot_dir, pretrained = True):
             
             # Read annotation PNG
             annotFile = os.path.join(annot_dir, seq, image)
-            objs = read_png_KITTI_MOTS(annotFile, pretrained)
+            objs = read_png_KITTI_MOTS(annotFile, pretrained, seg = False)
+            
+            record["annotations"] = objs
+                
+            dataset_dicts.append(record)
+            
+    return dataset_dicts
+
+def get_KITTI_MOTS_dataset(img_dir, annot_dir, pretrained = True):
+    """
+    This functions generates the dataset in COCO format given the path of the
+    KITTI-MOTS dataset images and annotations (bboxes  + instance seg.)
+
+    Parameters
+    ----------
+    img_dir : str
+        Images directory.
+    annot_dir : str
+        Annotations (pngs) directory.
+    pretrained : bool
+        True if the pretrained model with coco dataset will be used (will not be trained)
+
+    Returns
+    -------
+    dataset_dicts : list(dict)
+        Annotations in coco format.
+
+    """
+    
+    dataset_dicts = []
+    
+
+    # Get sequences
+    seqs = os.listdir(img_dir)
+    for seq in seqs:
+        # Get images
+        images = os.listdir(os.path.join(img_dir, seq))
+        for image in images:
+            record = {}
+            
+            filename = os.path.join(img_dir, seq, image)
+            height, width = cv2.imread(filename).shape[:2]
+            
+            record["file_name"] = filename
+            record["image_id"] = seq + "_" + image[:-4]
+            record["height"] = height
+            record["width"] = width
+            
+            # Read annotation PNG
+            annotFile = os.path.join(annot_dir, seq, image)
+            objs = read_png_KITTI_MOTS(annotFile, pretrained, seg = True)
             
             record["annotations"] = objs
                 
